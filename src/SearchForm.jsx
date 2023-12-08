76286c8e9186a6228e3e5d2641fa1c390091660d
Joseph Clay <josephc1417@gmail.com>
2023-12-08 13:35:02 -0600

    Unsplash API Register/Public Authenication

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if(!searchValue) return;
+    console.log(searchValue);
+  }
+  
   return (
-    <h2>SearchForm</h2>
+    <section>
+      <h1 className='title'>Unsplash Images</h1>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input type="text" className='form-input search-input' name="search" placeholder='Cat'  
+        />                      
+        <button 
+        type="submit" 
+        className='btn'>
+          search
+        </button>
+      </form>      
+    </section>
   )
 }
 
