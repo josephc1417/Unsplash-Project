f76009f875cea30f7c5a99e88cf0644488944760
Joseph Clay <josephc1417@gmail.com>
2023-12-08 14:07:00 -0600

    React Query Get Request

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url = 'https://api.unsplash.com/search/photos?client_id=digBziXjSqhJse76nbOIJIbeckbsZy_A7a8Bn8Flvtk&query=office';
+
 
 const Gallery = () => {
+  
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    }
+  })
+  console.log(response);
   return (
     <h2>Gallery</h2>
   )
