4f1dcdcf9bdf68198eafd614103079c7e57df667
Joseph Clay <josephc1417@gmail.com>
2023-12-07 22:18:33 -0600

    testing commit to main branch

--- src/context.jsx
+++ src/context.jsx
@@ -6,6 +6,14 @@ const AppContext = createContext();
 export const AppProvider = ({ children }) => {
      const greeting = 'hello';
 
+     const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+     const toggleTheme = () => {
+       const newDarkTheme = !isDarkTheme;
+       setIsDarkTheme(newDarkTheme);
+     }
+     }
+
      return (
           <AppContext.Provider value={{greeting}}>{children}</AppContext.Provider>
      );
